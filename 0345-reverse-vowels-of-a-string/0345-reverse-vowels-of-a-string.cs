a87f403915d69e025216e18bba17134b885c8430
Ji Sung Han <yhs24077@gmail.com>
2025-06-22 16:08:38 -0400

    Time: 3 ms (88.83%), Space: 45.3 MB (75.52%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cs
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cs
@@ -3,15 +3,19 @@ public class Solution {
       HashSet<char> vowels = new HashSet<char>(){
         'a', 'e', 'i','o','u', 'A', 'E', 'I', 'O', 'U'
       };
-      char[] chars =s.ToCharArray();
       int left = 0;
       int right = s.Length - 1;
-      while (left < right){
+      char[] chars = s.ToCharArray();
+      while( left < right){
+        //while left and right are diff.
         while(left < right && !vowels.Contains(chars[left])){
+          //then here we have left index with non vowels.
+          //then we loop
           left++;
 
         }
         while(left < right && !vowels.Contains(chars[right])){
+          //
           right--;
         }
         char temp = chars[left];
@@ -19,10 +23,10 @@ public class Solution {
         chars[right] = temp;
         left++;
         right--;
+
+
       }
       return new string(chars);
-
-
         
     }
 }
