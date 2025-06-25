a0ec5d360792e365ac38739c8f56afb9adc03a2c
Ji Sung Han <yhs24077@gmail.com>
2025-06-25 01:26:02 -0400

    Time: 0 ms (100%), Space: 43.2 MB (85.37%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
+      int n = cost.Length;
+      int[] dp = new int[n+1];
+      dp[0] = 0;
+      dp[1] = 0;
+      //dp[i] = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2]);
+      for(int i = 2; i <= n; i++){
+        dp[i] = Math.Min(
+          dp[i] = dp[i - 1] + cost[i - 1],
+          dp[i] = dp[i - 2] + cost[i - 2]
+        );
+
+
+      }
+      return dp[n];
+
+        
+    }
+}
