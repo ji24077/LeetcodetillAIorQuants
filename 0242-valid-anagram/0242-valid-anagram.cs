917a90dbbb29fcab06a1a1c6996aeba5fdfb3630
Ji Sung Han <yhs24077@gmail.com>
2025-06-29 19:51:04 -0400

    Time: 5 ms (70.22%), Space: 43.9 MB (45.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+      if(s.Length != t.Length){
+        return false;
+      }
+      
+      Dictionary<char, int> count = new Dictionary<char,int>();
+
+      foreach(char c in s){
+        if(!count.ContainsKey(c)){
+          count[c] = 0;
+        }
+        count[c]++;
+      }
+      foreach(char c in t){
+        if(!count.ContainsKey(c)){
+          return false;
+        }
+        count[c]--;
+        if ( count[c] < 0){
+          return false; // if it has more letters on s.
+        }
+      }
+      return true;
+
+        
+    }
+}
