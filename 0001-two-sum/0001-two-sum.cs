5323f7021814330359b6445c06bdce8a0671b09d
Ji Sung Han <yhs24077@gmail.com>
2025-06-29 21:45:41 -0400

    Time: 1 ms (98.5%), Space: 48.6 MB (75.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+      //create Dictionary<int,int> hmap = new Dction..
+      //loops the array of nums,
+
+
+      Dictionary<int, int> hmap = new Dictionary<int, int>();
+
+      for(int i = 0; i < nums.Length; i++){
+        int needed = target - nums[i];
+         if( hmap.ContainsKey(needed)){
+          return new int[] { hmap[needed],i};
+        }
+        if (!hmap.ContainsKey(nums[i])){
+          hmap[nums[i]] = i;
+
+        }
+       
+      }
+      return new int[] {-1, -1};
+        
+    }
+}
