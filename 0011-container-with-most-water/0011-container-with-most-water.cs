15acc7a5800e0f369cbe91063c6a79d38750c96e
Ji Sung Han <yhs24077@gmail.com>
2025-06-30 17:23:53 -0400

    Time: 1 ms (100%), Space: 63 MB (86.89%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxArea(int[] height) {
+      int left = 0;
+      int right = height.Length - 1;
+      int maxArea = 0;
+      while( left < right){
+        int area = (right - left) * Math.Min(height[left], height[right]);
+        maxArea = Math.Max(maxArea, area);
+        if( height[left] < height[right]){
+          left++;
+        }
+        else{
+          right--;
+        }
+
+      }
+      return maxArea;
+    }
+}
