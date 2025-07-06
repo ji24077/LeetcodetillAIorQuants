b39e1fb53e22312340620e1adc6045bd30fb22f5
Ji Sung Han <yhs24077@gmail.com>
2025-07-06 15:33:31 -0400

    Time: 1 ms (98.54%), Space: 49.2 MB (22.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -4,20 +4,19 @@ public class Solution {
       //loops the array of nums,
 
 
-      Dictionary<int, int> hmap = new Dictionary<int, int>();
+     Dictionary<int, int> hmap = new Dictionary<int, int>();
 
-      for(int i = 0; i < nums.Length; i++){
-        int needed = target - nums[i];
-         if( hmap.ContainsKey(needed)){
-          return new int[] { hmap[needed],i};
-        }
-        if (!hmap.ContainsKey(nums[i])){
-          hmap[nums[i]] = i;
-
-        }
-       
+     for(int i = 0; i <nums.Length; i++){
+      int needed = target - nums[i]; // 9 -2 = 7
+      if(hmap.ContainsKey(needed)){
+        return new int[] {hmap[needed], i};
       }
-      return new int[] {-1, -1};
+      if(!hmap.ContainsKey(nums[i])){
+        hmap[nums[i]] = i;
+      }
+
+     }
+     return new int[] {-1,-1};
         
     }
 }
