d1b6301a93f431ff1935dc5d275d552d25eae884
Ji Sung Han <yhs24077@gmail.com>
2025-07-24 19:58:53 -0400

    Time: 527 ms (66.03%), Space: 0B (100%) - LeetHub

--- 1877-find-followers-count/1877-find-followers-count.sql
+++ 1877-find-followers-count/1877-find-followers-count.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT f.user_id,
-(SELECT COUNT(*) FROM Followers f2 WHERE f2.user_id = f.user_id) AS followers_count
-FROM Followers f
-ORDER BY f.user_id;
+SELECT user_id, count(*) as followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id;
