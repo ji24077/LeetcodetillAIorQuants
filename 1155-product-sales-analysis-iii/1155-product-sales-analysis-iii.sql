d58ee286b13c0d974793bcc278bf47573746457a
Ji Sung Han <yhs24077@gmail.com>
2025-07-26 12:29:40 -0400

    Time: 690 ms (70.77%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,12 +1,11 @@
-# Write your MySQL query statement below
-SELECT
-  product_id,
-  year AS first_year,
-  quantity,
-  price
-FROM Sales
-WHERE (product_id, year) IN (
-  SELECT product_id, MIN(year) ## 100,200 | 2008,2011이있음.
-  FROM Sales
-  GROUP BY product_id
-);
+SELECT s.product_id,
+       s.year AS first_year,
+       s.quantity,
+       s.price
+FROM Sales AS s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year #min yr인 테이블과 조인해서 리턴 Inner임.
+    FROM Sales
+    GROUP BY product_id
+) AS m
+ON s.product_id = m.product_id AND s.year = m.first_year;
