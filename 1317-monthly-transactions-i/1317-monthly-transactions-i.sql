de89dd611132619be0eb3bc9b75c37447c943239
Ji Sung Han <yhs24077@gmail.com>
2025-07-26 12:10:13 -0400

    Time: 707 ms (16.41%), Space: 0B (100%) - LeetHub

---
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month,
+#--데이터 포맷은, 항상 trans_date,에서 yr, mth추출할떄사용.
+       country,
+       COUNT(*)                   AS trans_count, #--row count
+       SUM(state = 'approved')    AS approved_count,# --bool, apporved면1, ow 0
+       SUM(amount)                AS trans_total_amount,
+       SUM((state = 'approved') * amount) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
+ORDER BY month, country;
