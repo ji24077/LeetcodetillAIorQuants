953dbd65c03afe12ba9caddf69f0759d97847e4c
Ji Sung Han <yhs24077@gmail.com>
2025-07-26 13:16:57 -0400

    Time: 512 ms (83.66%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,7 +1,9 @@
-# Write your MySQL query statement below
-SELECT DISTINCT l1.num AS ConsecutiveNums
-FROM Logs l1, Logs l2, Logs l3
-WHERE l1.num = l2.num
-  AND l2.num = l3.num
-  AND l1.id = l2.id - 1
-  AND l1.id = l3.id - 2;
+SELECT DISTINCT num AS ConsecutiveNums   -- \U0001f539 중복 제거한 num 값을 ConsecutiveNums 컬럼 이름으로 출력
+FROM (
+  SELECT
+    num,                                           -- \U0001f539 현재 행의 값
+    LAG(num, 1) OVER (ORDER BY id) AS prev1,       -- \U0001f539 바로 이전 행의 num
+    LAG(num, 2) OVER (ORDER BY id) AS prev2        -- \U0001f539 두 번째 이전 행의 num
+  FROM Logs
+) AS sub
+WHERE num = prev1 AND num = prev2;                 -- \U0001f539 현재 값이 직전 2개의 값과 모두 같을 때만 선택
