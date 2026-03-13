f80053fb65d7b16ca15ca1166735dd2d90bc1259
Ji Sung Han <yhs24077@gmail.com>
2026-03-12 22:00:18 -0700

    Time: 25 ms (38.86%), Space: 19.7 MB (68.99%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,17 +1,23 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-      counts = {}
-      for ch in magazine:
-        counts[ch] = counts.get(ch, 0) + 1 
-        ## get(ch, value) = ch 가없음 value return.
-      
-      for ch in ransomNote:
-        if counts.get(ch, 0) == 0:
+      freq = {}
+
+      for char in magazine:
+        if char not in freq:
+          freq[char] = 0
+        freq[char] += 1
+
+      for char in ransomNote:
+        if char not in freq:
           return False
-        counts[ch] -=1
+        freq[char] -= 1
+
+        if freq[char] < 0:
+          return False
 
       return True
+
 
 
         
