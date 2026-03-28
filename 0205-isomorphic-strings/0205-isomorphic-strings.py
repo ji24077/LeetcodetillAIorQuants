23bc8c64dfce5a48d280b8de47f72cce36ac0928
Ji Sung Han <yhs24077@gmail.com>
2026-03-28 12:40:03 -0700

    Time: 3 ms (91.28%), Space: 19.2 MB (66.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,25 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        sMapt = {}
-        tMaps = {}
-        ##  s->t = e:a, g->d...
+        sMapt = {}  # s → t 매핑
+        tMaps = {}  # t → s 매핑
 
-        for i in range(len(s)):
-          ch_s = s[i]
+        for sc, tc in zip(s, t):  # zip으로 s,t 동시 순회 (range(len) 불필요)
 
-          ch_t = t[i]
+            # 둘 다 처음 보는 문자 → 매핑 등록
+            if sc not in sMapt and tc not in tMaps:
+                sMapt[sc] = tc
+                tMaps[tc] = sc
 
-          if ch_s in sMapt:
-            if sMapt[ch_s] != ch_t:
-              return False
+            # 기존 매핑과 다르면 False
+            elif sMapt.get(sc) != tc or tMaps.get(tc) != sc:
+                return False
 
-          else:
-            sMapt[ch_s] = ch_t
-
-          if ch_t in tMaps:
-            if tMaps[ch_t] != ch_s:
-              return False
-          else:
-            tMaps[ch_t] = ch_s
         return True
-
