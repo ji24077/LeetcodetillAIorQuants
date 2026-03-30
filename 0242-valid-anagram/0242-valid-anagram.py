2a3d39183f54a3b03aee6016472e64d337320dd3
Ji Sung Han <yhs24077@gmail.com>
2026-03-29 20:36:57 -0700

    Time: 10 ms (77.96%), Space: 19.2 MB (99.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-      count = {}
       if len(s) != len(t):
         return False
 
-      for char in s:
-        if char not in count:
-          count[char] = 0
-        count[char] += 1
-      
-      for char in t:
-        if char not in count:
+      freq_map = {}
+      for word in s:
+        freq_map[word] = freq_map.get(word, 0) + 1
+
+      for word in t:
+        
+        if word not in freq_map:
           return False
-        count[char] -= 1
-        if count[char] < 0:
+        freq_map[word] -= 1
+
+        if freq_map[word] < 0:
           return False
+        
       return True
-          
-
-
-            
