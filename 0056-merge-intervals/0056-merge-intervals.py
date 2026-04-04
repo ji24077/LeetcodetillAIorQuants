cfb49d04657f941280378866b79c94ab0c587a33
Ji Sung Han <yhs24077@gmail.com>
2026-04-03 18:15:56 -0700

    Time: 7 ms (71.5%), Space: 22.4 MB (77.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-      intervals.sort(key = lambda x:x[0]) # sort based on start
+      intervals.sort(key= lambda x: x[0])
+
       result = []
+      result.append(intervals[0])
+      
 
-      result.append(intervals[0]) #add [1,3] first
+
+
       for i in range(1, len(intervals)):
-        last = result[-1] # current result's last interval, for first its [1,3]
-        curr = intervals[i] # current interval [2,6]
-
-        if last[1] >= curr[0]: #last[1] = 3, curr[0] = 2,
-          last[1] = max(last[1], curr[1]) # 즉 겹치면, last의 [1,3]-> [1,6]으로 끝을갱신
-
+        curr = intervals[i] # [2,6]
+        last = result[-1]
+        if last[1] >= curr[0]:
+          #ie) 3>=2
+          last[1] = max(last[1], curr[1])
         else:
-          # if not, we just keep the same interval
-          result.append(curr)
+          result.append(intervals[i])
+
       return result
+
+          
 
