84e4b82bc01dad89d98bb0a5f0b917b1ff03a81e
Ji Sung Han <yhs24077@gmail.com>
2026-04-03 17:43:31 -0700

    Time: 0 ms (100%), Space: 19.3 MB (42.87%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,27 +1,33 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-       
-        result = []
+      if not nums:
+        return []
 
-        if not nums:
-            return result
+      start = nums[0] # 0
+      result = []
 
-        start = nums[0]
+      for i in range(1, len(nums)):
+        if nums[i] - nums[i - 1] != 1:
+          if start == nums[i - 1]:
+          #if its not consecutive, ie) 4-2 = 2 != 1, 
+            result.append(str(start)) # append 7 will not happen, 
+          #its because we can not add start since it might cont
+          else:
+            result.append(f"{start}->{nums[i-1]}")
+          #0->2, 4->5
+          start = nums[i] # start = 4
+      # if start != nums[-1]:
+      #   #since last index cannot be reach by index
+      if start == nums[-1]:
+        result.append(str(start))       # 혼자면 "7"
+      else:
+        result.append(f"{start}->{nums[-1]}")  # 연속이면 "7->9"
+      return result
 
-        for i in range(1, len(nums)):
-            # 끊기는 순간에만 구간 확정
-            if nums[i] - nums[i-1] != 1: # later index i, and before index i-1
-                if nums[i-1] != start:
-                  result.append(f"{start}->{nums[i-1]}")
-                else:
-                  result.append(str(nums[i - 1]))
-                start = nums[i]
 
 
-        # 마지막 구간 처리
-        if start == nums[-1]:
-            result.append(str(start))
-        else:
-            result.append(f"{start}->{nums[-1]}")
 
-        return result
+
+
+
+      
